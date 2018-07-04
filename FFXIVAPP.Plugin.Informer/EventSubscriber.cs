c8d45c342619aa4c2577c609f949c1954efb391c
Ryan Wilson <syndicated.life@gmail.com>
2018-07-03 22:18:35 -0700

    license, dependencies, net 462 updates

--- FFXIVAPP.Plugin.Informer/EventSubscriber.cs
+++ FFXIVAPP.Plugin.Informer/EventSubscriber.cs
@@ -1,64 +1,58 @@
-﻿// FFXIVAPP.Plugin.Informer ~ EventSubscriber.cs
-// 
-// Copyright © 2007 - 2017 Ryan Wilson - All Rights Reserved
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EventSubscriber.cs" company="SyndicatedLife">
+//   Copyright(c) 2018 Ryan Wilson &amp;lt;syndicated.life@gmail.com&amp;gt; (http://syndicated.life/)
+//   Licensed under the MIT license. See LICENSE.md in the solution root for full license information.
+// </copyright>
+// <summary>
+//   EventSubscriber.cs Implementation
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using FFXIVAPP.IPluginInterface.Events;
-using FFXIVAPP.Plugin.Informer.ViewModels;
-using Sharlayan.Core;
+namespace FFXIVAPP.Plugin.Informer {
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
 
-namespace FFXIVAPP.Plugin.Informer
-{
-    public static class EventSubscriber
-    {
-        public static void Subscribe()
-        {
-            Plugin.PHost.NewConstantsEntity += OnNewConstantsEntity;
-            //Plugin.PHost.NewChatLogEntry += OnNewChatLogEntry;
-            Plugin.PHost.NewMonsterEntries += OnNewMonsterEntries;
-            Plugin.PHost.NewNPCEntries += OnNewNPCEntries;
-            Plugin.PHost.NewPCEntries += OnNewPCEntries;
-            Plugin.PHost.NewPlayerEntity += OnNewPlayerEntity;
-            Plugin.PHost.NewTargetEntity += OnNewTargetEntity;
-            //Plugin.PHost.NewPartyEntries += OnNewPartyEntries;
+    using FFXIVAPP.Common.Core.Constant;
+    using FFXIVAPP.IPluginInterface.Events;
+    using FFXIVAPP.Plugin.Informer.ViewModels;
+
+    using Sharlayan.Core;
+
+    public static class EventSubscriber {
+        public static void Subscribe() {
+            Plugin.PHost.ConstantsUpdated += OnConstantsUpdated;
+
+            // Plugin.PHost.NewChatLogEntry += OnNewChatLogEntry;
+            Plugin.PHost.MonsterItemsUpdated += OnMonsterItemsUpdated;
+            Plugin.PHost.NPCItemsUpdated += OnNewNPCItemsUpdated;
+            Plugin.PHost.PCItemsUpdated += OnNewPCItemsUpdated;
+            Plugin.PHost.CurrentPlayerUpdated += OnCurrentPlayerUpdated;
+            Plugin.PHost.TargetInfoUpdated += OnTargetInfoUpdated;
+
+            // Plugin.PHost.NewPartyEntries += OnNewPartyEntries;
         }
 
-        public static void UnSubscribe()
-        {
-            Plugin.PHost.NewConstantsEntity -= OnNewConstantsEntity;
-            //Plugin.PHost.NewChatLogEntry -= OnNewChatLogEntry;
-            Plugin.PHost.NewMonsterEntries -= OnNewMonsterEntries;
-            Plugin.PHost.NewNPCEntries -= OnNewNPCEntries;
-            Plugin.PHost.NewPCEntries -= OnNewPCEntries;
-            Plugin.PHost.NewPlayerEntity -= OnNewPlayerEntity;
-            Plugin.PHost.NewTargetEntity -= OnNewTargetEntity;
-            //Plugin.PHost.NewPartyEntries -= OnNewPartyEntries;
+        public static void UnSubscribe() {
+            Plugin.PHost.ConstantsUpdated -= OnConstantsUpdated;
+
+            // Plugin.PHost.NewChatLogEntry -= OnNewChatLogEntry;
+            Plugin.PHost.MonsterItemsUpdated -= OnMonsterItemsUpdated;
+            Plugin.PHost.NPCItemsUpdated -= OnNewNPCItemsUpdated;
+            Plugin.PHost.PCItemsUpdated -= OnNewPCItemsUpdated;
+            Plugin.PHost.CurrentPlayerUpdated -= OnCurrentPlayerUpdated;
+            Plugin.PHost.TargetInfoUpdated -= OnTargetInfoUpdated;
+
+            // Plugin.PHost.NewPartyEntries -= OnNewPartyEntries;
         }
 
-        #region Subscriptions
-
-        private static void OnNewConstantsEntity(object sender, ConstantsEntityEvent constantsEntityEvent)
-        {
+        private static void OnConstantsUpdated(object sender, ConstantsEntityEvent constantsEntityEvent) {
             // delegate event from constants, not required to subsribe, but recommended as it gives you app settings
-            if (sender == null)
-            {
+            if (sender == null) {
                 return;
             }
-            var constantsEntity = constantsEntityEvent.ConstantsEntity;
+
+            ConstantsEntity constantsEntity = constantsEntityEvent.ConstantsEntity;
             Constants.AutoTranslate = constantsEntity.AutoTranslate;
             Constants.ChatCodes = constantsEntity.ChatCodes;
             Constants.Colors = constantsEntity.Colors;
@@ -71,105 +65,96 @@ namespace FFXIVAPP.Plugin.Informer
             PluginViewModel.Instance.EnableHelpLabels = Constants.EnableHelpLabels;
         }
 
-        //private static void OnNewChatLogEntry(object sender, ChatLogEntryEvent chatLogEntryEvent)
-        //{
-        //    // delegate event from chat log, not required to subsribe
-        //    // this updates 100 times a second and only sends a line when it gets a new one
-        //    if (sender == null)
-        //    {
-        //        return;
-        //    }
-        //    var chatLogEntry = chatLogEntryEvent.ChatLogEntry;
-        //}
+        private static void OnCurrentPlayerUpdated(object sender, CurrentPlayerEvent currentPlayerEvent) {
+            // delegate event from player info from ram, not required to subsribe
+            // this is for YOU and includes all your stats and your agro list with hate values as %
+            // this updates 10x a second and only sends data when the newly read data is differen than what was previously sent
+            if (sender == null) {
+                return;
+            }
 
-        private static void OnNewMonsterEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
-        {
+            CurrentPlayer currentPlayer = currentPlayerEvent.CurrentPlayer;
+            XIVInfoViewModel.Instance.AgroEntries = new ObservableCollection<EnmityItem>(currentPlayer.EnmityItems ?? new List<EnmityItem>());
+        }
+
+        // private static void OnNewChatLogEntry(object sender, ChatLogEntryEvent chatLogEntryEvent)
+        // {
+        // // delegate event from chat log, not required to subsribe
+        // // this updates 100 times a second and only sends a line when it gets a new one
+        // if (sender == null)
+        // {
+        // return;
+        // }
+        // var chatLogEntry = chatLogEntryEvent.ChatLogEntry;
+        // }
+        private static void OnMonsterItemsUpdated(object sender, ActorItemsEvent actorItemsEvent) {
             // delegate event from monster entities from ram, not required to subsribe
             // this updates 10x a second and only sends data if the items are found in ram
             // currently there no change/new/removed event handling (looking into it)
-            if (sender == null)
-            {
+            if (sender == null) {
                 return;
             }
-            var monsterEntities = actorEntitiesEvent.ActorEntities;
+
+            ConcurrentDictionary<uint, ActorItem> monsterEntities = actorItemsEvent.ActorItems;
             XIVInfoViewModel.Instance.CurrentMonsters = monsterEntities;
         }
 
-        private static void OnNewNPCEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
-        {
+        private static void OnNewNPCItemsUpdated(object sender, ActorItemsEvent actorItemsEvent) {
             // delegate event from npc entities from ram, not required to subsribe
             // this list includes anything that is not a player or monster
             // this updates 10x a second and only sends data if the items are found in ram
             // currently there no change/new/removed event handling (looking into it)
-            if (sender == null)
-            {
+            if (sender == null) {
                 return;
             }
-            var npcEntities = actorEntitiesEvent.ActorEntities;
+
+            ConcurrentDictionary<uint, ActorItem> npcEntities = actorItemsEvent.ActorItems;
             XIVInfoViewModel.Instance.CurrentNPCs = npcEntities;
         }
 
-        private static void OnNewPCEntries(object sender, ActorEntitiesEvent actorEntitiesEvent)
-        {
+        private static void OnNewPCItemsUpdated(object sender, ActorItemsEvent actorItemsEvent) {
             // delegate event from player entities from ram, not required to subsribe
             // this updates 10x a second and only sends data if the items are found in ram
             // currently there no change/new/removed event handling (looking into it)
-            if (sender == null)
-            {
+            if (sender == null) {
                 return;
             }
-            var pcEntities = actorEntitiesEvent.ActorEntities;
+
+            ConcurrentDictionary<uint, ActorItem> pcEntities = actorItemsEvent.ActorItems;
             XIVInfoViewModel.Instance.CurrentPCs = pcEntities;
         }
 
-        private static void OnNewPlayerEntity(object sender, PlayerEntityEvent playerEntityEvent)
-        {
-            // delegate event from player info from ram, not required to subsribe
-            // this is for YOU and includes all your stats and your agro list with hate values as %
-            // this updates 10x a second and only sends data when the newly read data is differen than what was previously sent
-            if (sender == null)
-            {
-                return;
-            }
-            var playerEntity = playerEntityEvent.PlayerEntity;
-            XIVInfoViewModel.Instance.AgroEntries = new ObservableCollection<EnmityEntry>(playerEntity.EnmityEntries ?? new List<EnmityEntry>());
-        }
-
-        private static void OnNewTargetEntity(object sender, TargetEntityEvent targetEntityEvent)
-        {
+        private static void OnTargetInfoUpdated(object sender, TargetInfoEvent targetInfoEvent) {
             // delegate event from target info from ram, not required to subsribe
             // this includes the full entities for current, previous, mouseover, focus targets (if 0+ are found)
             // it also includes a list of upto 16 targets that currently have hate on the currently targeted monster
             // these hate values are realtime and change based on the action used
             // this updates 10x a second and only sends data when the newly read data is differen than what was previously sent
-            if (sender == null)
-            {
+            if (sender == null) {
                 return;
             }
-            var targetEntity = targetEntityEvent.TargetEntity;
 
-            if (ActorEntity.CurrentUser != null)
-            {
-                XIVInfoViewModel.Instance.CurrentUser = ActorEntity.CurrentUser;
+            TargetInfo targetInfo = targetInfoEvent.TargetInfo;
+
+            if (ActorItem.CurrentUser != null) {
+                XIVInfoViewModel.Instance.CurrentUser = ActorItem.CurrentUser;
             }
 
-            XIVInfoViewModel.Instance.EnmityEntries = new ObservableCollection<EnmityEntry>(targetEntity.EnmityEntries ?? new List<EnmityEntry>());
-            XIVInfoViewModel.Instance.CurrentTarget = targetEntity.CurrentTarget ?? new ActorEntity();
-            XIVInfoViewModel.Instance.MouseOverTarget = targetEntity.MouseOverTarget ?? new ActorEntity();
-            XIVInfoViewModel.Instance.FocusTarget = targetEntity.FocusTarget ?? new ActorEntity();
-            XIVInfoViewModel.Instance.PreviousTarget = targetEntity.PreviousTarget ?? new ActorEntity();
+            XIVInfoViewModel.Instance.EnmityEntries = new ObservableCollection<EnmityItem>(targetInfo.EnmityItems ?? new List<EnmityItem>());
+            XIVInfoViewModel.Instance.CurrentTarget = targetInfo.CurrentTarget ?? new ActorItem();
+            XIVInfoViewModel.Instance.MouseOverTarget = targetInfo.MouseOverTarget ?? new ActorItem();
+            XIVInfoViewModel.Instance.FocusTarget = targetInfo.FocusTarget ?? new ActorItem();
+            XIVInfoViewModel.Instance.PreviousTarget = targetInfo.PreviousTarget ?? new ActorItem();
         }
 
-        //private static void OnNewPartyEntries(object sender, PartyEntitiesEvent partyEntitiesEvent)
-        //{
-        //    // delegate event that shows current party basic info
-        //    if (sender == null)
-        //    {
-        //        return;
-        //    }
-        //    var partyEntities = partyEntitiesEvent.PartyEntities;
-        //}
-
-        #endregion
+        // private static void OnNewPartyEntries(object sender, PartyEntitiesEvent partyEntitiesEvent)
+        // {
+        // // delegate event that shows current party basic info
+        // if (sender == null)
+        // {
+        // return;
+        // }
+        // var partyEntities = partyEntitiesEvent.PartyEntities;
+        // }
     }
 }
